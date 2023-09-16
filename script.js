cc789b222033cea63368895849025c61dc2974a5
HARSH KHATRI <83015486+harshkhatri2220@users.noreply.github.com>
2023-09-17 01:57:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let randomHit;
+let score=0;
+const makeBubble = () =>{
+    let bubble="";
+    let randomInteger = 0;
+    
+    
+    for(let i=0 ; i<112 ; i++)
+    {
+        randomInteger = Math.floor(Math.random()*10);
+        bubble += `<div id="bub">${randomInteger}</div>`
+         
+    }
+    document.querySelector('#bottom').innerHTML=bubble;
+}
+makeBubble();
+
+const setTimer = () =>{
+    let timer = 60;
+    let timerInterval = setInterval(() => {
+        if(timer>0)
+        {
+            timer--;
+            document.querySelector("#timer").textContent=timer;
+        }
+        else
+        {
+            clearInterval(timerInterval);
+document.querySelector("#bottom").innerHTML = `<div class="bottomAfterGameOver">
+<h1 style="color: white;">Score  : ${score} </h1>
+  <h1 style="color: white;">Game Over</h1>
+  <p style="color: white;">Click to play again</p>
+  </div>
+  `;
+
+
+        }
+    }, 1000);
+}
+setTimer();
+
+const getNewHit = () =>{
+    randomHit=Math.floor(Math.random()*10);
+    document.querySelector("#randomHit").textContent=randomHit;
+}
+getNewHit();
+
+
+document.querySelector("#bottom").addEventListener("click" , (details)=>{
+    bubbleClicked = (Number(details.target.textContent));
+    if(bubbleClicked == randomHit)
+    {
+    score+=1;
+    document.querySelector("#score").textContent=score;
+    }
+    makeBubble();
+    getNewHit();
+
+})
